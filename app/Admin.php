82064be7a55ecc0599227fe245e1a15086a3114c
Muhammed-Salah <19gcs01@meaec.edu.in>
2020-07-28 21:46:36 +0530

    first commit

--- app/Admin.php
+++ app/Admin.php
@@ -2,6 +2,7 @@
 
 namespace App;
 
+use App\Notifications\AdminResetPasswordNotification;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -39,4 +40,8 @@ class Admin extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+    public function sendPasswordResetNotification($token)
+    {
+        $this->notify(new AdminResetPasswordNotification($token));
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -40,5 +40,9 @@ Route::prefix('admin')->group(function () {
     Route::get('/register','Auth\AdminRegisterController@showRegisterForm')->name('admin.register');
     Route::post('/register','Auth\AdminRegisterController@register')->name('admin.register.submit');
 
-
+    //Password reset routes
+    Route::get('/password/reset','Auth\AdminForgotPasswordController@showLinkRequestForm')->name('admin.password.request');
+    Route::post('/password/email','Auth\AdminForgotPasswordController@SendResetLinkEmail')->name('admin.password.email');
+    Route::get('/password/reset/{token}','Auth\AdminResetPasswordController@showResetForm')->name('admin.password.reset');
+    Route::post('/password/reset','Auth\AdminResetPasswordController@reset')->name('admin.password.update');
 });
